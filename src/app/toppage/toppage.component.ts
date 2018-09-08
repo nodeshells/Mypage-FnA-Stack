95262b1f362a86fb4d1e754084ee2e2e94514c5d
kenta <kenta.job09@gmail.com>
2018-09-09 01:17:13 +0900

    実装予定のコンポーネントを追加

---
+++ src/app/toppage/toppage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toppage',
+  templateUrl: './toppage.component.html',
+  styleUrls: ['./toppage.component.css']
+})
+export class ToppageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
