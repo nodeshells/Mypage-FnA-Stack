cd49fb7d639b0bd6e4756eae6ac0744042cdcbf5
kenta <kenta.job09@gmail.com>
2018-09-09 01:15:01 +0900

    ルーティング先の変更

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { Routes, RouterModule, CanActivate, PreloadingStrategy, PreloadAllModules } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
+import { ToppageComponent } from './toppage/toppage.component';
 
 
 const myRoutes = [
-  { path: '', component: HelloComponent },
-  { path: '**', component: HelloComponent },
+  { path: '', component: ToppageComponent },
+  { path: '**', component: ToppageComponent },
 ];
 
 
