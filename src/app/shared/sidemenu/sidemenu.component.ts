ed54899b2055205353c63ba8603f7f6a40f9a058
kenta <kenta.job09@gmail.com>
2018-09-09 23:45:45 +0900

    サイドメニューコンポーネントのリンクを変更

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,19 +1,43 @@
-import { Component, ChangeDetectorRef, Inject, Injectable, OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef, Inject, Injectable, OnInit, DoCheck, OnChanges } from '@angular/core';
 import { SharedModule } from '../shared.module';
 import { Router, NavigationEnd } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { Location } from '../../../../node_modules/@angular/common';
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styleUrls: ['./sidemenu.component.css']
 })
-export class SidemenuComponent implements OnInit {
+export class SidemenuComponent implements OnInit, OnChanges {
 
-  constructor(private router: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) { }
+  public activebox = {
+    top: false,
+    profile: false
+  };
+
+  constructor(private router: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private location: Location) {
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.activebox.top = false;
+    this.activebox.profile = false;
+
+    switch (String(this.location.path)) {
+      case '/top':
+        this.activebox.top = true;
+        break;
+
+      case '/profile':
+        this.activebox.profile = true;
+        break;
+      default:
+        break;
+    }
   }
 
 }
