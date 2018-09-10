ef672a9af9f72b39b4709c5877da8d4738ac000f
kenta <kenta.job09@gmail.com>
2018-09-10 23:35:16 +0900

    トップページの文言を変更

--- src/app/toppage/toppage.component.ts
+++ src/app/toppage/toppage.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../shared/shared.service';
 
 @Component({
   selector: 'app-toppage',
   templateUrl: './toppage.component.html',
-  styleUrls: ['./toppage.component.css']
+  styleUrls: ['./toppage.component.css'],
+  providers: [SharedService]
 })
 export class ToppageComponent implements OnInit {
 
-  constructor() { }
+  isMobile;
+  parallaxsize_pc = '400';
+  parallaxsize_m = '300';
+
+  constructor(private sharedservice: SharedService) {
+    this.sharedservice.displaysizedetect().subscribe((isMobile: boolean) => {
+      this.isMobile = isMobile;
+    });
+  }
 
   ngOnInit() {
   }
