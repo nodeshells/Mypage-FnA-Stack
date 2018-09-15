39ca1bf4a7d74c28985250abd4770d77e1fd9676
kenta <kenta.job09@gmail.com>
2018-09-15 17:02:54 +0900

    サイト管理画面を編集中

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { TopComponent } from './top/top.component';
 import { myRoutes } from './admin-routing.module';
 import { RouterModule } from '../../../node_modules/@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(myRoutes)
+    RouterModule.forChild(myRoutes),
+    SharedModule
   ],
   declarations: [TopComponent]
 })
--- src/app/admin/top/top.component.ts
+++ src/app/admin/top/top.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/authguard/user.service';
 
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
-  styleUrls: ['./top.component.css']
+  styleUrls: ['./top.component.css'],
+  providers: [UserService]
 })
 export class TopComponent implements OnInit {
+  user_photo_url = '';
+  user_name = '';
 
-  constructor() { }
+  constructor(private user: UserService) { }
 
   ngOnInit() {
+    this.getUserdata();
+  }
+
+  async getUserdata() {
+    const user = await this.user.getCurrentUser();
+    this.user_photo_url = user.photoURL;
+    this.user_name = user.displayName;
   }
 
 }
