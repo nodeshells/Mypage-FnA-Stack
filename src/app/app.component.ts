f0346d9f3aab40194d9eade3e9c52585ebe95e29
kenta <kenta.job09@gmail.com>
2019-08-20 21:49:10 +0900

    [Update]画面の右下にfabボタンでメニューを実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
-import {Subject} from 'rxjs';
-import {DomSanitizer} from '@angular/platform-browser';
-import {SharedService} from './shared/shared.service';
-import {Storage} from '@ionic/storage';
-import {MenuController} from '@ionic/angular';
-import {AuthService} from './shared/authguard/auth.service';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SharedService } from './shared/shared.service';
+import { Storage } from '@ionic/storage';
+import { MenuController } from '@ionic/angular';
+import { AuthService } from './shared/authguard/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +14,17 @@ import {AuthService} from './shared/authguard/auth.service';
 })
 export class AppComponent implements OnDestroy, OnInit, AfterViewInit {
   themeState$: Subject<String>;
+  sideMenu = [
+    { labelName: 'TOPページ', iconName: 'home', routerLink: '/top' },
+    { labelName: 'マイページ', iconName: 'happy', routerLink: '/profile' },
+    { labelName: 'ブログ', iconName: 'text', routerLink: '/blog' },
+    { labelName: '職務経歴', iconName: 'filing', routerLink: '' },
+    { labelName: '技術プレビュー', iconName: 'construct', routerLink: '' },
+    { labelName: '管理者ページ', iconName: 'glasses', routerLink: '/admin' },
+  ];
 
   constructor(private domSanitizer: DomSanitizer, private sharedservice: SharedService, private storage: Storage,
-              private menu: MenuController, private authService: AuthService) {
+    private menu: MenuController, private authService: AuthService) {
     this.sharedservice.initTheme();
     this.themeState$ = this.sharedservice.themesubject;
 
@@ -36,10 +44,6 @@ export class AppComponent implements OnDestroy, OnInit, AfterViewInit {
   toggleTheme() {
     const themeState = this.sharedservice.toggleTheme(this.sharedservice.themeState);
     this.storage.set('Theme', themeState).then();
-  }
-
-  closeMenu() {
-    this.menu.close().then();
   }
 
 }
