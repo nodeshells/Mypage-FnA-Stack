a67f72ac58ee437dc392db1f6a8c7e315f5556fa
kenta <kenta.job09@gmail.com>
2020-01-21 11:43:04 +0900

    [Delete&Update]いらない依存系のパッケージやファイルを削除&メタ情報の設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import {Subject} from 'rxjs';
-import {DomSanitizer} from '@angular/platform-browser';
+import {DomSanitizer, Meta} from '@angular/platform-browser';
 import {SharedService} from './shared/shared.service';
 import {Storage} from '@ionic/storage';
 import {MenuController} from '@ionic/angular';
@@ -23,12 +23,23 @@ export class AppComponent implements OnDestroy, OnInit, AfterViewInit {
     // { labelName: '技術プレビュー', iconName: 'construct', routerLink: '' },
     {labelName: '管理者ページ', iconName: 'glasses', routerLink: '/admin'},
   ];
+  // メタデータの設定
+  metaObject = {
+    title: 'Kenta Technology Lab',
+    desc: 'Webの技術に将来性を見出すしがないエンジニアのホームページです。',
+    type: 'website',
+    site_url: 'https://kenta-technology-lab.work',
+    image_url: 'assets/mysite_ogp.jpg',
+    twitter_card: 'summary',
+    twitter_site: '@nodeshells',
+    twitter_creator: '@nodeshells'
+  };
 
   constructor(private domSanitizer: DomSanitizer, private sharedservice: SharedService, private storage: Storage,
-              private menu: MenuController, public authService: AuthService) {
+              private menu: MenuController, public authService: AuthService, private metaService: Meta) {
     this.sharedservice.initTheme();
     this.themeState$ = this.sharedservice.themesubject;
-
+    this.setMetaTag();
   }
 
   ngOnInit() {
@@ -40,6 +51,17 @@ export class AppComponent implements OnDestroy, OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
 
+  }
+
+  setMetaTag() {
+    this.metaService.addTag({property: 'og:title', content: this.metaObject.title});
+    this.metaService.addTag({property: 'og:description', content: this.metaObject.desc});
+    this.metaService.addTag({property: 'og:type', content: this.metaObject.type});
+    this.metaService.addTag({property: 'og:url', content: this.metaObject.site_url});
+    this.metaService.addTag({property: 'og:image', content: this.metaObject.image_url});
+    this.metaService.addTag({property: 'twitter:card', content: this.metaObject.twitter_card});
+    this.metaService.addTag({property: 'twitter:site', content: this.metaObject.twitter_site});
+    this.metaService.addTag({property: 'twitter:creator', content: this.metaObject.twitter_creator});
   }
 
   toggleTheme() {
