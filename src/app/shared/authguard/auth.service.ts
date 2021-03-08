25d6bfa66ed020f699e8fd301c9f5ca7f03ee894
nodeshells <kenta.job09@gmail.com>
2021-03-08 22:47:07 +0900

    [Update]resolve return true

--- src/app/shared/authguard/auth.service.ts
+++ src/app/shared/authguard/auth.service.ts
@@ -8,40 +8,40 @@ import {AngularFireAuth} from '@angular/fire/auth';
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
-  constructor(
-      public afAuth: AngularFireAuth, private router: Router
-  ) {
-  }
+    constructor(
+        public afAuth: AngularFireAuth, private router: Router
+    ) {
+    }
 
-  doGoogleLogin() {
-    return new Promise<any>((resolve, reject) => {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      provider.addScope('profile');
-      provider.addScope('email');
-      this.afAuth.signInWithPopup(provider)
-          .then(res => {
-            resolve(res);
-          }, err => {
-            reject(err);
-          });
-    });
-  }
+    doGoogleLogin() {
+        return new Promise<any>((resolve, reject) => {
+            const provider = new firebase.auth.GoogleAuthProvider();
+            provider.addScope('profile');
+            provider.addScope('email');
+            this.afAuth.signInWithPopup(provider)
+                .then(res => {
+                    resolve(res);
+                }, err => {
+                    reject(err);
+                });
+        });
+    }
 
-  checkLoginState(): Observable<User | null> {
-    return this.afAuth.user;
-  }
+    checkLoginState(): Observable<User | null> {
+        return this.afAuth.user;
+    }
 
-  doLogout() {
-    return new Promise((resolve, reject) => {
-      if (firebase.auth().currentUser) {
-        this.afAuth.signOut();
-        this.router.navigate(['/top']);
-        resolve();
-      } else {
-        reject();
-      }
-    });
-  }
+    doLogout() {
+        return new Promise((resolve, reject) => {
+            if (firebase.auth().currentUser) {
+                this.afAuth.signOut();
+                this.router.navigate(['/top']);
+                resolve(true);
+            } else {
+                reject();
+            }
+        });
+    }
 
 
 }
